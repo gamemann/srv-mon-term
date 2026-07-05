50666da47074e6ce49a6d01bcebb6c17d4229ba7
Christian Deacon <christianmdeacon@gmail.com>
2026-07-05 14:46:50 -0400

    Fix crash related to truncate_str().

--- src/util/string.rs
+++ src/util/string.rs
@@ -1,7 +1,11 @@
 pub fn truncate_str(s: &str, max: usize) -> String {
-    if s.len() <= max {
+    let char_count = s.chars().count();
+
+    if char_count <= max {
         s.to_string()
     } else {
-        format!("{}…", &s[..max.saturating_sub(1)])
+        // Ex: Like thi...
+        let truncated: String = s.chars().take(max.saturating_sub(1)).collect();
+        format!("{}…", truncated)
     }
 }
